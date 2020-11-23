1b1749a960f15cf9146cec44cdfc5be47d177d03
Felipe <felipe@talavera.io>
2020-11-23 18:47:00 -0300

    demo test

---
+++ src/test/java/simpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class simpleTest {
+    @Test
+    public void hello() {
+        assertEquals(42, Integer.sum(19, 23));
+    }
+}
